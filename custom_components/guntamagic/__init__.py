dce573b0c6a1476a6e749536e5b2fb72674fc0fe
eurochallenge-esc <131952775+OldGuyDevGithub@users.noreply.github.com>
2025-03-07 00:13:12 +0100

    Update __init__.py

--- custom_components/guntamagic/__init__.py
+++ custom_components/guntamagic/__init__.py
@@ -6,17 +6,29 @@ import logging
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Guntamagic from a config entry."""
-    _LOGGER.debug("Guntamagic async_setup_entry aufgerufen.")
-    hass.data.setdefault("guntamagic", {})
+    _LOGGER.debug(" async_setup_entry in __init__.py aufgerufen.")
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Remove a config entry."""
-    _LOGGER.debug("Guntamagic async_unload_entry aufgerufen.")
+    _LOGGER.debug("async_unload_entry in __init__.py aufgerufen")
+
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
     return True
 
