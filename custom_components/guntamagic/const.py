785ce3990045ada9131baa8f3ec8d29048fb9776
eurochallenge-esc <131952775+OldGuyDevGithub@users.noreply.github.com>
2025-03-11 08:50:11 +0100

    config labeling

--- custom_components/guntamagic/const.py
+++ custom_components/guntamagic/const.py
@@ -1,4 +1,4 @@
 DOMAIN = "guntamagic"
-CONF_IP_ADDRESS = "ip_address"
-CONF_KEY = "key"
-CONF_NAME = "name"
+CONF_IP_ADDRESS = "IP address of the entity (e.g. 192.168.1.10)"
+CONF_KEY = "Your key for the log gasifier interface provided by guntamatic"
+CONF_NAME = "Name of the entity (e.g. BMK20)"
