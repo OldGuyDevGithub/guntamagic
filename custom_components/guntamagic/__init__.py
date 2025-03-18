4c302c2a30074110706d0afec14a7234742aa7c1
eurochallenge-esc <131952775+OldGuyDevGithub@users.noreply.github.com>
2025-03-18 21:02:23 +0100

    Update __init__.py

--- custom_components/guntamagic/__init__.py
+++ custom_components/guntamagic/__init__.py
@@ -16,9 +16,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    hass.async_create_task(
-        await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+
     return True
 
 
